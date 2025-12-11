c37f53f1341d92b7738e21bc0cd47a31c16efb4b
Garest <olehhoncharenko98@gmail.com>
2025-12-11 11:35:57 +0200

    Removed permission for tabs

--- manifest.config.ts
+++ manifest.config.ts
@@ -62,6 +62,6 @@ export default function getManifest() {
       service_worker: "assets/background.js",
       type: "module",
     },
-    permissions: ["storage", "tabs"],
+    permissions: ["storage"],
   } as Manifest.WebExtensionManifest;
 }
