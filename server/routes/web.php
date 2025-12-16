3ea76baf712ac3586d4770291fd9f58024e18da4
Garest <olehhoncharenko98@gmail.com>
2025-12-16 11:33:26 +0200

    Update web.php

--- server/routes/web.php
+++ server/routes/web.php
@@ -4,9 +4,6 @@ use App\Http\Controllers\AppController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', [HomeController::class, 'index'])->name('home');
-// Route::get('/privacy', [PrivacyController::class, 'index'])->name('privacy');
-
 Route::prefix('auth')->controller(AuthController::class)->group(function () {
     Route::get('/login/twitch', 'logIn');
     Route::get('/callback/twitch', 'callback');
