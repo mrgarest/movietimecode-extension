a02d35f47fde17decfe8c587a9a933fb657a6f90
Garest <olehhoncharenko98@gmail.com>
2026-01-09 23:05:14 +0200

    Update TimecodeEditorResource.php

--- server/app/Http/Resources/Timecode/TimecodeEditorResource.php
+++ server/app/Http/Resources/Timecode/TimecodeEditorResource.php
@@ -17,6 +17,7 @@ class TimecodeEditorResource extends SuccessResource
         return [
             'id' => $this->resource->id,
             'movie_id' => $this->resource->movieId,
+            'duration' => $this->resource->duration,
             'segments' => TimecodeSegmentResource::collection($this->resource->segments),
         ];
     }
