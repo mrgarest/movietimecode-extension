8fe39da960a0dd89542f4414e7e28a8bc3e109ee
Garest <olehhoncharenko98@gmail.com>
2026-01-09 22:08:59 +0200

    Update web.php

--- server/routes/web.php
+++ server/routes/web.php
@@ -7,12 +7,11 @@ use Illuminate\Support\Facades\Route;
 // Outdated authorization method
 Route::prefix('auth')->controller(AuthController::class)->group(function () {
     Route::get('/login/twitch', 'extension');
-    Route::get('/callback/twitch', 'callback');
 });
 
-Route::prefix('login')->controller(AuthController::class)->group(function () {
-    Route::get('/extension', 'extension');
-    Route::get('/callback', 'callback');
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/login/extension', 'extension');
+    Route::get('/auth/callback', 'callback');
 });
 
 // React route
